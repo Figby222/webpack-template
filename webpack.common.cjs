eeee63a1140bd85289cb25eccd13a0f6f73292fd
Figby222 <figby222@outlook.com>
2024-04-24 15:31:05 -0700

    Add webpack common, dev, and prod configs

---
+++ webpack.common.cjs
@@ -0,0 +1,41 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+module.exports = {
+  entry: {
+    app: "./src/index.js",
+  },
+  output: {
+    filename: "[name].bundle.js",
+    path: path.resolve(__dirname, "dist"),
+    clean: true,
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "./src/template.html",
+      title: "Production",
+      inject: "head",
+      scriptLoading: "defer",
+    }),
+  ],
+  module: {
+    rules: [
+        {
+            test: /\.css$/i,
+            use: ["style-loader", "css-loader"],
+        },
+        {
+            test: /\.(?:js|mjs|cjs)$/,
+            exclude: /node_modules/,
+            use: {
+              loader: "babel-loader",
+              options: {
+                presets: [
+                  ["@babel/preset-env", { targets: "defaults" }]
+                ]
+              }
+            }
+        }
+    ]
+  },
+};
