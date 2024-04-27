035075e36eb94e22a15d1d6fd409fec18bde4c47
Figby222 <figby222@outlook.com>
2024-04-27 05:32:24 -0700

    Add image loading

--- webpack.common.cjs
+++ webpack.common.cjs
@@ -35,7 +35,11 @@ module.exports = {
                 ]
               }
             }
-        }
+        },
+        {
+          test: /\.(png|svg|jpg|jpeg|gif)$/i,
+          type: "asset/resource",
+        },
     ]
   },
 };
